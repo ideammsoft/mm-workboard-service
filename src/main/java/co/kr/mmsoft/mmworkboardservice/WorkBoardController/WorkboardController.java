b5d0ddea833290609dd47323c7a23645d79b57eb
ideakslee <ideakslee@gmail.com>
2026-03-29 13:04:19 +0900

    localhost 수정

--- src/main/java/co/kr/mmsoft/mmworkboardservice/WorkBoardController/WorkboardController.java
+++ src/main/java/co/kr/mmsoft/mmworkboardservice/WorkBoardController/WorkboardController.java
@@ -5,6 +5,7 @@ import co.kr.mmsoft.mmworkboardservice.mapper.WorkboardMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
